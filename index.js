8c9f0d77775c918df48d3b687a75dba5db870147
Vicente Monsalve <vicente.monsalve@gmail.com>
2022-09-20 22:51:40 -0300

    cambios: hola mudno con express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo')
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.json({msg: 'Hola Mundo'})
+})
+
+
+const port = 1337
+app.listen(port, () =>{
+    console.log(`El servidor esta funcionando en http://localhost:${port}`)
+})
